635b7c3d3540a5038fdd48e4ce84a1993aad24d9
Jeni <jeniferfcr@gmail.com>
2020-10-04 18:09:47 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reto-comunal';
+  buttons: { name: string; icon: string; }[];
+ 
+
+
+  constructor() {
+    this.buttons = [
+      {
+        name: "Reserva de salas", 
+        icon: "assets/icon1.png"
+      },
+      {
+        name: "Reporte de Problemas", 
+        icon: "assets/icon2.png"
+      },
+      {
+        name: "Mi equipo", 
+        icon: "assets/icon3.png"
+      },
+      {
+        name: "Comunicados", 
+        icon: "assets/icon5.png"
+      },
+      {
+        name: "Ficha de sintomatología", 
+        icon: "assets/icon4.png"
+      }
+    ]
+
+  }
 }
